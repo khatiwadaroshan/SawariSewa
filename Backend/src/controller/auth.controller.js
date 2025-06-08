c2c5397ae838d18edd541b48f4175b6522b23ab6
khatiwadaroshan <roshankhatiwada058@gmail.com>
2025-06-08 12:55:01 +0545

    completed login

--- Backend/src/controller/auth.controller.js
+++ Backend/src/controller/auth.controller.js
@@ -2,6 +2,8 @@ import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import { generateToken } from "../utils/generationtoken.js";
 
+// for signup
+
 export const signup = async (req, res) => {
   try {
     const { fullname, password, email } = req.body;
@@ -44,4 +46,41 @@ export const signup = async (req, res) => {
     console.log("signup error:", error.message);
     res.status(500).json({ message: "Internal server error" });
   }
+
 };
+
+// for login
+
+export const login = async(req,res) => {
+  const {email, password} = req.body;
+  try{
+  
+     if(!email || !password){
+      return res.status(400).json({message:"Invalid credentials"})
+     }
+     const user = await User.findOne({email})
+     if(!user){
+      return res.status(400).json({message:"no user found"})
+     }
+     const isMatch = await bcrypt.compare(password,user.password)
+     
+     if(!isMatch){
+      return res.status(400).json({message:"invalid password"})
+     }
+
+     generateToken(user._id,res);
+     res.status(200).json({
+      _id: user._id,
+      email:user.email,
+      password:user.password
+     })
+
+     
+  }
+  catch(error){
+    console.log("login error",error.message);
+    res.status.json({message:"invalid email and password"})
+  }
+
+
+}
